c5cb5106cc14aea3a416e91fa837c3c05f1ecdf3
Dakewallington <thenoblebrotherhood@gmail.com>
2016-12-23 14:17:43 -0600

    component runs

--- admin/SketchfabViewer.php
+++ admin/SketchfabViewer.php
@@ -1,14 +1,1 @@
-<?php
-
-    /**
-    * @package Joomla.Administrator
-    * @subpackage comp_helloworld
-    * 
-    * @copyright Copyright (C) 2016 - 2030 Open Source Mattters, INC All right reserved.
-    * @license GNU General Public Liceense version 2 or Later;  See LICENSE.txt
-    */
-
-    //NO direct access to this file
-    defined('_JEXEC') or die('Restricted Access');
-
-?>
+Hello World administration
